acf23fad9754b31779425b856b106f94adf1135e
William Emfinger <waemfinger@gmail.com>
2022-11-04 20:47:36 -0500

    updated boxart and added metadatafile to repo.

--- boxart/source/resize.bash
+++ boxart/source/resize.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -maxdepth 1 -iname "*.jpg" | xargs -L1 -I{} convert -resize 100x100 "{}" ../"{}"
+find . -maxdepth 1 -iname "*.jpg" | xargs -L1 -I{} convert -resize 100x "{}" ../"{}"
