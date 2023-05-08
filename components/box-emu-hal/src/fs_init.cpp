3b331f2ec41cd63b6388818c8a28479972586310
William Emfinger <waemfinger@gmail.com>
2023-05-08 12:53:38 -0500

    feat(mmap): remove mmap-ed partition * Remove littlefs for the filesystem (only use uSD from now on) * Remove parititon for cart data * Change to using a SPIRAM heap-allocated 4MB memory sector for the cart instead of the partition * Read the whole cart into ram in one call (speeds up rom loading)

--- components/box-emu-hal/src/fs_init.cpp
+++ components/box-emu-hal/src/fs_init.cpp
@@ -2,42 +2,6 @@
 
 #include "format.hpp"
 
-#if CONFIG_ROM_STORAGE_LITTLEFS
-static esp_vfs_littlefs_conf_t conf = {
-  .base_path = MOUNT_POINT,
-  .partition_label = "littlefs",
-  .format_if_mount_failed = true,
-  .dont_mount = false,
-};
-
-static void littlefs_init() {
-  // Use settings defined above to initialize and mount LittleFS filesystem.
-  // Note: esp_vfs_littlefs_register is an all-in-one convenience function.
-  esp_err_t ret = esp_vfs_littlefs_register(&conf);
-
-  if (ret != ESP_OK) {
-    if (ret == ESP_FAIL) {
-      fmt::print("Failed to mount or format filesystem\n");
-    } else if (ret == ESP_ERR_NOT_FOUND) {
-      fmt::print("Failed to find LittleFS partition\n");
-    } else {
-      fmt::print("Failed to initialize LittleFS ({})\n", esp_err_to_name(ret));
-    }
-    return;
-  }
-
-  size_t total = 0, used = 0;
-  ret = esp_littlefs_info(conf.partition_label, &total, &used);
-  if (ret != ESP_OK) {
-    fmt::print("Failed to get LittleFS partition information ({})\n",
-               esp_err_to_name(ret));
-  } else {
-    fmt::print("Partition size: total: {}, used: {}\n", total, used);
-  }
-}
-#endif
-
-#if CONFIG_ROM_STORAGE_SDCARD
 static void sdcard_init() {
   esp_err_t ret;
 
@@ -105,12 +69,7 @@ static void sdcard_init() {
   // Card has been initialized, print its properties
   sdmmc_card_print_info(stdout, card);
 }
-#endif
 
 void fs_init() {
-#if CONFIG_ROM_STORAGE_LITTLEFS
-  littlefs_init();
-#elif CONFIG_ROM_STORAGE_SDCARD
   sdcard_init();
-#endif
 }
--- components/box-emu-hal/src/mmap.cpp
+++ components/box-emu-hal/src/mmap.cpp
@@ -1,57 +1,28 @@
 #include "mmap.hpp"
+#include "esp_heap_caps.h"
 
-const esp_partition_t* cart_partition;
-spi_flash_mmap_handle_t hrom;
+static uint8_t* romdata = nullptr;
 
 void init_memory() {
-  // Initialize NVS, needed for BT
-  esp_err_t ret = nvs_flash_init();
-  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-    fmt::print("Erasing NVS flash...\n");
-    ESP_ERROR_CHECK(nvs_flash_erase());
-    ret = nvs_flash_init();
-  }
-  ESP_ERROR_CHECK(ret);
-
-  // get the romdata (cart) partition, type 0x40, subtype 1 (partitions.csv)
-  cart_partition = esp_partition_find_first((esp_partition_type_t)0x40, (esp_partition_subtype_t)1, NULL);
-  if (!cart_partition) {
-    fmt::print(fg(fmt::color::red), "Couldn't find cart_partition!\n");
+  // allocate memory for the ROM and make sure it's on the SPIRAM
+  romdata = (uint8_t*)heap_caps_malloc(4*1024*1024, MALLOC_CAP_8BIT | MALLOC_CAP_SPIRAM);
+  if (romdata == nullptr) {
+    fmt::print(fg(fmt::terminal_color::red), "ERROR: Couldn't allocate memory for ROM!\n");
   }
 }
 
 size_t copy_romdata_to_cart_partition(const std::string& rom_filename) {
-  esp_err_t err;
-  // erase the existing rom (if any) in the cart_partition
-  err = esp_partition_erase_range(cart_partition, 0, cart_partition->size);
-  if (err != ESP_OK) {
-    fmt::print("Couldn't erase cart_partition!\n");
-  }
   // load the file data and iteratively copy it over
   std::ifstream romfile(rom_filename, std::ios::binary | std::ios::ate); //open file at end
   if (!romfile.is_open()) {
     fmt::print("Error: ROM file does not exist\n");
     return 0;
   }
-
   size_t filesize = romfile.tellg(); // get size from current file pointer location;
   romfile.seekg(0, std::ios::beg); //reset file pointer to beginning;
-
-  size_t block_size = 1024*4; // 4K
-  size_t bytes_written = 0;
-  char block[block_size];
-  for (size_t offset=0; offset < filesize; offset += block_size) {
-    size_t read_size = std::min(filesize - offset, block_size);
-    romfile.read(block, read_size);
-    err = esp_partition_write(cart_partition, offset, block, read_size);
-    if (err != ESP_OK) {
-      fmt::print("Couldn't write to cart_partition, offset: {}, read_size: {}!\n", offset, read_size);
-    }
-    bytes_written += read_size;
-  }
-  fmt::print("Copied {} bytes to cart_partition\n", bytes_written);
+  romfile.read((char*)(romdata), filesize);
   romfile.close();
-  return bytes_written;
+  return filesize;
 }
 
 extern "C" uint8_t *osd_getromdata() {
@@ -59,13 +30,6 @@ extern "C" uint8_t *osd_getromdata() {
 }
 
 uint8_t *get_mmapped_romdata() {
-  esp_err_t err;
-  uint8_t* romdata = nullptr;
-  err = esp_partition_mmap(cart_partition, 0, 3*1024*1024, (esp_partition_mmap_memory_t)SPI_FLASH_MMAP_DATA, (const void**)&romdata, &hrom);
-  if (err != ESP_OK) {
-    fmt::print("Couldn't map cart_partition!\n");
-    return nullptr;
-  }
   fmt::print("Initialized. ROM@{}\n", fmt::ptr(romdata));
   return romdata;
 }
