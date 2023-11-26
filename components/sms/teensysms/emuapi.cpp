2c28cddae8587898e69a191b68f7d14860e3c1b2
William Emfinger <waemfinger@gmail.com>
2023-11-26 15:59:10 -0700

    feat(sms): added sms emulator from MCUME project

---
+++ components/sms/teensysms/emuapi.cpp
@@ -0,0 +1,113 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+
+#include <cstdio>
+
+extern "C" {
+  #include "emuapi.h"
+  #include "iopins.h"
+}
+
+// #include "bmpjoy.h"
+// #include "bmpvbar.h"
+
+// #include "esp_event.h"
+
+void emu_init(void)
+{
+}
+
+
+void emu_printf(char * text)
+{
+  printf("%s\n",text);
+}
+
+
+void emu_printi(int val)
+{
+  printf("%d\n",val);
+}
+
+void * emu_Malloc(int size)
+{
+  void * retval =  malloc(size);
+  if (!retval) {
+    printf("failled to allocate %d\n",size);
+  }
+  else {
+    printf("could allocate %d\n",size); 
+  }
+  
+  return retval;
+}
+
+void emu_Free(void * pt)
+{
+  free(pt);
+}
+
+static unsigned short palette16[PALETTE_SIZE];
+static int fskip=0;
+
+void emu_SetPaletteEntry(unsigned char r, unsigned char g, unsigned char b, int index)
+{
+  if (index<PALETTE_SIZE) {
+    //printf("%d: %d %d %d\n", index, r,g,b);
+    palette16[index] = RGBVAL16(r,g,b);    
+  }
+}
+
+void emu_DrawVsync(void)
+{
+  //printf("sync %d\n",skip);  
+  fskip += 1;
+  fskip &= VID_FRAME_SKIP;
+}
+
+void emu_DrawLine(unsigned char * VBuf, int width, int height, int line) 
+{
+  if (fskip==0) {
+    // TODO: use box-hal to send frame data to screen
+    // tft.writeLine(width,height,line, VBuf, palette16);
+  }
+}  
+
+void emu_DrawScreen(unsigned char * VBuf, int width, int height, int stride) 
+{  
+  if (fskip==0) {
+    // TODO: use box-hal to send frame data to screen
+    // tft.writeScreen(width,height-TFT_VBUFFER_YCROP,stride, VBuf+(TFT_VBUFFER_YCROP/2)*stride, palette16);
+  }
+}
+
+int emu_FrameSkip(void)
+{
+  return fskip;
+}
+
+void * emu_LineBuffer(int line)
+{
+  // TODO: use box-hal to get line data from screen buffer
+  // return (void*)tft.getLineBuffer(line);
+  return nullptr;
+}
+
+#include "AudioPlaySystem.h"
+extern AudioPlaySystem audio;
+
+void emu_sndInit() {
+}
+
+void emu_sndPlaySound(int chan, int volume, int freq)
+{
+  if (chan < 6) {
+    audio.sound(chan, freq, volume); 
+  } 
+}
+
+void emu_sndPlayBuzz(int size, int val) {
+  //mymixer.buzz(size,val);  
+}
