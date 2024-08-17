a80c56e8a96793007ada8b5e4a31c4b67710eb17
William Emfinger <waemfinger@gmail.com>
2024-08-16 22:14:44 -0500

    update to use latest apis - use new haptics apis and such

--- main/main.cpp
+++ main/main.cpp
@@ -6,7 +6,6 @@
 #include <vector>
 #include <stdio.h>
 
-#include "drv2605.hpp"
 #include "logger.hpp"
 #include "task_monitor.hpp"
 #include "timer.hpp"
@@ -21,12 +20,11 @@
 using namespace std::chrono_literals;
 
 extern "C" void app_main(void) {
-  espp::Logger logger({.tag = "esp-box-emu", .level = espp::Logger::Verbosity::DEBUG});
+  espp::Logger logger({.tag = "esp-box-emu", .level = espp::Logger::Verbosity::INFO});
   logger.info("Bootup");
 
   // initialize the hardware abstraction layer
   BoxEmu &emu = BoxEmu::get();
-  emu.set_log_level(espp::Logger::Verbosity::DEBUG);
   espp::EspBox &box = espp::EspBox::get();
   logger.info("Running on {}", box.box_type());
   logger.info("Box Emu version: {}", emu.version());
@@ -62,27 +60,10 @@ extern "C" void app_main(void) {
     return;
   }
 
-  std::error_code ec;
-
-  auto &external_i2c = emu.external_i2c();
-  espp::Drv2605 haptic_motor(espp::Drv2605::Config{
-      .device_address = espp::Drv2605::DEFAULT_ADDRESS,
-      .write = std::bind(&espp::I2c::write, &external_i2c, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3),
-      .read_register = std::bind(&espp::I2c::read_at_register, &external_i2c, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4),
-      .motor_type = espp::Drv2605::MotorType::LRA
-    });
-  // we're using an LRA motor, so select th LRA library.
-  haptic_motor.select_library(espp::Drv2605::Library::LRA, ec);
-
-  auto play_haptic = [&haptic_motor]() {
-    std::error_code ec;
-    haptic_motor.start(ec);
-  };
-  auto set_waveform = [&haptic_motor](int waveform) {
-    std::error_code ec;
-    haptic_motor.set_waveform(0, (espp::Drv2605::Waveform)(waveform), ec);
-    haptic_motor.set_waveform(1, espp::Drv2605::Waveform::END, ec);
-  };
+  if (!emu.initialize_haptics()) {
+    logger.error("Failed to initialize haptics!");
+    return;
+  }
 
   logger.info("initializing gui...");
 
@@ -90,9 +71,9 @@ extern "C" void app_main(void) {
 
   // initialize the gui
   Gui gui({
-      .play_haptic = play_haptic,
-      .set_waveform = set_waveform,
-      .log_level = espp::Logger::Verbosity::DEBUG
+      .play_haptic = [&emu]() { emu.play_haptic_effect(); },
+      .set_waveform = [&emu](uint8_t waveform) { emu.set_haptic_effect(waveform); },
+      .log_level = espp::Logger::Verbosity::WARN
     });
 
   print_heap_state();
@@ -105,15 +86,13 @@ extern "C" void app_main(void) {
     }
 
     // have broken out of the loop, let the user know we're processing...
-    haptic_motor.start(ec);
+    emu.play_haptic_effect();
 
-    // Now pause the LVGL gui
     gui.pause();
-    display->pause();
 
     auto maybe_selected_rom = gui.get_selected_rom();
     if (maybe_selected_rom.has_value()) {
-      auto selected_rom = *maybe_selected_rom.value();
+      auto selected_rom = maybe_selected_rom.value();
       logger.info("Selected rom:\n\t{}", selected_rom);
 
       print_heap_state();
@@ -122,6 +101,7 @@ extern "C" void app_main(void) {
       {
         std::unique_ptr<Cart> cart(make_cart(selected_rom, display));
 
+        display->pause();
         if (cart) {
           while (cart->run());
         } else {
